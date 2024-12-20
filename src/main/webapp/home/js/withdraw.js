65335b1cd4b8162d7b967b41963a672e1a41e271
hayoon-shin <ssh3324@gmail.com>
2024-12-20 22:28:51 +0900

    mvc 1 개인홈페이지 테스팅 파일 이동

--- src/main/webapp/home/js/withdraw.js
+++
@@ -1,9 +0,0 @@
-function pwdCheck(){
-let value = document.withForm.pwd.value;
-if(value === ""){
-alert("비밀번호를 입력해 주세요.");
-document.withForm.pwd.focus();
-return;
-}
-document.withForm.submit();
-}
